b9c9734f728329e8ba5816a7a50f157ddecbb2d0
Eylon Shmilovich <eylonshm@gmail.com>
2023-08-23 01:13:15 +0300

    Update Server root HTML

--- server/index.js
+++ server/index.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import dalleRoutes from './routes/dalle.routes.js';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import dalleRoutes from './routes/dalle.routes.js'
+import html from './rootHtml.js'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+const app = express()
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
 
-app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/dalle', dalleRoutes)
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello from DALL.E' });
-});
+  res.type('html').send(html)
+})
 
-app.listen(8080, () => console.log('Server has started on port 8080'));
+app.listen(8080, () => console.log('Server has started on port 8080'))
---
+++ server/rootHtml.js
@@ -0,0 +1,17 @@
+const html = `<!DOCTYPE html>
+<html>
+<body>
+<div style='display: flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column'>
+    
+<h1>Customise Shirt API</h1>
+
+<p>Just use the different routes plz.</p>
+
+</body>
+</html>
+`
+
+export default html
